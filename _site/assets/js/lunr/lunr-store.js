6640e74eaa553c1afc382ce4f064ccf695e0f8a4
Abhijeet Mishra <abhijeet.m@igidr.ac.in>
2026-07-03 17:55:46 +0530

    update site content

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,41 @@
+var store = [{
+        "title": "software_package1",
+        "excerpt":"Summary This package takes in two numbers and adds them together! ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/code/software_package1",
+        "teaser":null},{
+        "title": "Kerala Chapter 1: Arrival",
+        "excerpt":"The tour to Kerala began when I applied to CDS Trivandrum. My paper got accepted, and finally, I had the chance to visit Kerala. I had previously asked Subhabarata to plan a trip, but it failed every time. So I grabbed this opportunity with both hands. To add to my...","categories": ["kerala"],
+        "tags": [],
+        "url": "http://localhost:4000/kerala/kerala-ch1/",
+        "teaser":null},{
+        "title": "Pricing, Technology, and Enforcement in Markets with Digital Piracy: A Hotelling Approach",
+        "excerpt":"Abstract We analyze the strategic interaction between an original content provider and a pirate firm in a spatial Hotelling framework. The model accounts for technological difficulty and probabilistic enforcement as determinants of consumer adoption and firm profits. Under uniform distributions, we derive closed-form equilibrium prices, technological effort, and market shares....","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/publications/publication%201",
+        "teaser":null},{
+        "title": "Young Scholar Conference (2025)",
+        "excerpt":"Presented the paper: Pricing, Technology, and Enforcement in Markets with Digital Piracy: A Hotelling Approach. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/talks/talk1/",
+        "teaser":null},{
+        "title": "Crossroads (2026)",
+        "excerpt":"Presented the paper: Pricing, Technology, and Enforcement in Markets with Digital Piracy: A Hotelling Approach. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/talks/talk2/",
+        "teaser":null},{
+        "title": "Teaching Assistant-Macroeconomics I (2024)",
+        "excerpt":"Helped in grading answer sheets. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/teaching/course1",
+        "teaser":null},{
+        "title": "Teaching Assistant-Mathematics for Economist (2025)",
+        "excerpt":"Helped in grading answer sheets. Took some tutorial classes on logic and real analysis. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/teaching/course1",
+        "teaser":null},{
+        "title": "Teaching Assistant-Microeconomics II (2025)",
+        "excerpt":"Helped in grading answer sheets. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/teaching/course1",
+        "teaser":null}]
